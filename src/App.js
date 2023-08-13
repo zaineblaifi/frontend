08678dd1f35df6b63a704a99cb35fe12cf3bfbf8
zaineblaifi <112078103+zaineblaifi@users.noreply.github.com>
2023-08-13 07:46:37 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import Home from './Home';
+import Register from './register';
+import Login from "./login";
+import Profile from "./profile";
+import Enregvideo from './Enreg_video';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/register' element={<Register />}></Route>
+      <Route path='/login' element={<Login />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/Enreg_video' element={<Enregvideo/>}></Route>
+
+
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
